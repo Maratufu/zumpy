bb9bf50a0df4078c47be038f91236c773db5be9d
iekecan <42848933+iekecan@users.noreply.github.com>
2018-08-30 16:47:34 -0300

    Update zumpy.py
    Mudando a msg do bot.

--- zumpy.py
+++ zumpy.py
@@ -5,7 +5,7 @@ updater = Updater(token=os.environ['TOKEN'])
 
 def hello(bot, update):
     update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+        'Olá {}, no momento estou em manuntenção!'.format(update.message.from_user.first_name))
 
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
