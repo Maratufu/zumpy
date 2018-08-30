2cf617c603edca5af753f268fae7ec639c261eb8
Bender <42433811+Maratufu@users.noreply.github.com>
2018-08-30 13:53:40 -0300

    Update zumpy.py

--- zumpy.py
+++ zumpy.py
@@ -8,7 +8,7 @@ def hello(bot, update):
         'Hello {}'.format(update.message.from_user.first_name))
 
 
-updater = Updater('YOUR TOKEN HERE')
+updater = Updater(tk)
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
