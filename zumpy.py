f754a4484e39916815fbe6fe0b5ce1d6d58ded90
Bender <42433811+Maratufu@users.noreply.github.com>
2018-08-30 15:26:06 -0300

    Update zumpy.py

--- zumpy.py
+++ zumpy.py
@@ -5,7 +5,7 @@ updater = Updater(token=os.environ['TOKEN'])
 
 def hello(bot, update):
     update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+        'Hello {0} {1}'.format(update.message.from_user.first_name, os.environ['TOKEN']))
 
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
